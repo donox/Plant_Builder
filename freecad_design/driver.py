Minor fixes, rename variables, remove assumption about measurement units being mm.  System now unitless except when printing sizes.

freecad_design/driver.py
@@ -78,6 +78,7 @@ class Driver(object):
         if not do_cuts and remove_existing:  # if do_cuts, there is no generated display
             remove_string += "|.+|e.+|E.+|f.+|A.+"
         doc_list = self.doc.findObjects(Name=remove_string)  # obj names start with l,e,L,E,f,K
+        print(f"DOC LIST LEN: {len(doc_list)}")
         for item in doc_list:
             if item.Label != 'Parms_Master':
                 try:
@@ -167,13 +168,23 @@ class Driver(object):
 
     def build_place_list(self):
         print(f"BUILD PLACE LIST")
+        min_max = [[0, 0], [0, 0], [0, 0]]
+        def find_min_max(base):
+            for i in range(3):
+                if base[i] < min_max[i][0]:
+                    min_max[i][0] = np.round(base[i], 3)
+                if base[i] > min_max[i][1]:
+                    min_max[i][1] = np.round(base[i], 3)
         cuts_file_name = self.get_parm("place_file")
         cuts_file = open(cuts_file_name, "w+")
         cuts_file.write("Wafer Placement:\n\n\n")
         global_placement = FreeCAD.Placement(FreeCAD.Vector(0, 0, 0), FreeCAD.Rotation(0, 0, 0))
 
         for nbr, segment in enumerate(self.segment_list):
-            global_placement = segment.print_construction_list(nbr, cuts_file, global_placement)
+            global_placement = segment.print_construction_list(nbr, cuts_file, global_placement, find_min_max)
+        min_max_str = f"\nGlobal Min Max:\n\tX: {min_max[0][0]} - {min_max[0][1]}, "
+        min_max_str += f"Y: {min_max[1][0]} - {min_max[1][1]}, Z: {min_max[2][0]} - {min_max[2][1]}"
+        cuts_file.write(f"{min_max_str}")
 
         cuts_file.close()
 

freecad_design/segment.py
@@ -70,10 +70,12 @@ class Segment(object):
 
     def get_wafer_parameters(self):
         if self.lift_angle != 0:  # leave as simple cylinder if zero
+            la = self.lift_angle / 2
+            oh = self.outside_height / 2
             # Assume origin at center of ellipse, x-axis along major axis, positive to outside.
-            self.helix_radius = np.math.cos(self.lift_angle) / np.math.sin(self.lift_angle) * self.outside_height
+            self.helix_radius = oh / np.math.tan(la)
             # print(f"SET RADIUS: {self.helix_radius}, Lift: {self.lift_angle}, Height: {self.outside_height}")
-            self.inside_height = (self.helix_radius - self.cylinder_diameter) * np.math.tan(self.lift_angle)
+            self.inside_height = (self.helix_radius - self.cylinder_diameter) * np.math.tan(la) * 2
 
     def get_lcs_top(self):
         print(f"LCS TOP: {self.lcs_top.Label}")
@@ -175,14 +177,14 @@ class Segment(object):
         parm_str = f"\n\nCut list for segment: {segment_no}\n"
         parm_str += f"Lift Angle: {np.round(np.rad2deg(self.lift_angle), 2)} degrees\n"
         parm_str += f"Rotation Angle: {np.rad2deg(self.rotation_angle)} degrees\n"
-        parm_str += f"Outside Wafer Height: {np.round(self.outside_height / 25.4, 2)} in\n"
+        parm_str += f"Outside Wafer Height: {np.round(self.outside_height, 2)} in\n"
         if self.inside_height:
-            parm_str += f"Inside Wafer Height: {np.round(self.inside_height / 25.4, 2)} in\n"
+            parm_str += f"Inside Wafer Height: {np.round(self.inside_height, 2)} in\n"
         else:
             parm_str += f"Inside Wafer Height: NONE\n"
-        parm_str += f"Cylinder Diameter: {np.round(self.cylinder_diameter / 25.4, 2)} in\n"
+        parm_str += f"Cylinder Diameter: {np.round(self.cylinder_diameter, 2)} in\n"
         if self.helix_radius:
-            parm_str += f"Helix Radius: \t{np.round(self.helix_radius / 25.4, 2)} in\n"
+            parm_str += f"Helix Radius: \t{np.round(self.helix_radius, 2)} in\n"
         else:
             parm_str += f"Helix Radius: NONE\n"
         cuts_file.write(parm_str)
@@ -210,7 +212,7 @@ class Segment(object):
             cuts_file.write(f"{str1} {str2};    Done: _____\n")
             current_position = int((current_position - step_size + 180) % 360)
 
-    def print_construction_list(self, segment_no, cons_file, global_placement):
+    def print_construction_list(self, segment_no, cons_file, global_placement, find_min_max):
         parm_str = f"\nConstruction list for segment: {segment_no}\n"
         parm_str += f"Lift Angle: {np.round(np.rad2deg(self.lift_angle), 2)} degrees\n"
         parm_str += f"Rotation Angle: {np.rad2deg(self.rotation_angle)} degrees\n"
@@ -233,16 +235,19 @@ class Segment(object):
             cons_file.write(f"This segment was reconstructed thus there is no wafer list")
             return None
         for wafer_num, wafer in enumerate(self.wafer_list):
+            # if wafer_num:
+            #     prior_top = top_lcs_place
             top_lcs_place = wafer.get_top().Placement
             global_loc = global_placement.multiply(top_lcs_place)
-            num_str = str(wafer_num + 1)       # Make one-based for conformance with practice
+            num_str = str(wafer_num + 1)       # Make one-based for conformance with  in shop
             local_x = position_to_str(top_lcs_place.Base.x)
             global_x = position_to_str(global_loc.Base.x)
             local_y = position_to_str(top_lcs_place.Base.y)
             global_y = position_to_str(global_loc.Base.y)
             local_z = position_to_str(top_lcs_place.Base.z)
             global_z = position_to_str(global_loc.Base.z)
-            # Seems to be a long way around to get the lcs z-axis s vector
+            find_min_max(global_loc.Base)
+            # Seems to be a long way around to get the lcs z-axis's vector
             lcso = FreeCAD.activeDocument().getObject(wafer.get_lcs_top().Label)
             vec = FreeCAD.Vector(0, 0, 1)
             vec2 = lcso.getGlobalPlacement().Rotation.multVec(vec)
@@ -251,6 +256,17 @@ class Segment(object):
             str1 += f" with Angle: {np.round(np.rad2deg(angle), 1)}\n"
             str1 += f"\t\tat Global: [{global_x}, {global_y}, {global_z}]\n"
             cons_file.write(str1)
+            # if wafer_num:
+            #     # relocate to global space using prior wafer and compute direction and distance.
+            #     relocate = prior_top.inverse()
+            #     relative_position = relocate.multiply(top_lcs_place)
+            #     x_pos = position_to_str(relative_position.Base.x)
+            #     y_pos = position_to_str(relative_position.Base.y)
+            #     z_pos = position_to_str(relative_position.Base.z)
+            #     str2 = f"\t\tRelative placement:\t[{x_pos}, {y_pos}, {z_pos}\n]"
+            #     euler_angles = relative_position.Rotation.getYawPitchRoll()
+            #     str2 += f"\t\tEuler Angles:\t{euler_angles}\n"
+            #     cons_file.write(str2)
         return global_loc
 
     @staticmethod

freecad_design/structurehelix.py
@@ -65,7 +65,7 @@ class StructureHelix(object):
             e_name = 'e' + str(total_wafer_count)
             self.lcs_writer.writerow([e_name, lcs_temp.Placement])
             e_name += '_top'
-            print(f"LIFT in WRITE: lift angle - {np.round(np.rad2deg(self.lift_angle), 2)}, j - {j}")
+            # print(f"LIFT in WRITE: lift angle - {np.round(np.rad2deg(self.lift_angle), 2)}, j - {j}")
             if j == 0:
                 # lift_lcs(lcs_temp, 0, self.helix_radius, self.outside_height / 2)   # Presume cylinder for half
                 lift_lcs(lcs_temp, self.lift_angle, self.cylinder_diameter, self.outside_height, "CE")

freecad_design/wafer.py
@@ -113,32 +113,33 @@ def lift_lcs(lcs, lift_angle, cylinder_diameter, outside_height, lcs_type):
              "CE": ("CC2", "CE2"),
              "EC": ("EC2", "CC2"),
              "EE": ("EC2", "CE2")}
-    used_parts = parts[lcs_type]
+    parts_used = parts[lcs_type]
     h2 = outside_height / 2
     d2 = cylinder_diameter / 2
     la = lift_angle / 2
     result_vec = []
     for i in range(2):
-        if "EC2" == used_parts[i]:
+        # print(f"PARTS: {parts_used}, LCS: {lcs.Label}, i: {i}")
+        if "EC2" == parts_used[i]:
             del_x = -d2 * np.sin(la)
             del_z = h2 - d2 * np.tan(la)
             result_vec.append(FreeCAD.Vector(del_x, 0, del_z))
-            # print(f"EC  x: {np.round(del_x, 3)}, z: {np.round(del_z, 3)} res: {result_vec[i]}")
-        if "CE2" == used_parts[i]:
+            # print(f"EC  x: {np.round(del_x, 3)}, z: {np.round(del_z, 3)}, {i}: {parts_used}")
+        if "CE2" == parts_used[i]:
             del_x = -d2 * np.sin(la)
             del_z = h2 - d2 * np.tan(la)
             result_vec.append(FreeCAD.Vector(del_x, 0, del_z))
-            # print(f"CE  x: {np.round(del_x, 3)}, z: {np.round(del_z, 3)} res: {result_vec[i]}")
-        if "CC2" == used_parts[i]:
+            # print(f"CE  x: {np.round(del_x, 3)}, z: {np.round(del_z, 3)}  {i}: {parts_used}")
+        if "CC2" == parts_used[i]:
             del_x = 0
-            del_z = h2
+            del_z = 0              # removes cylindrical part
             result_vec.append(FreeCAD.Vector(del_x, 0, del_z))
             # print(f"CC  x: {np.round(del_x, 3)}, z: {np.round(del_z, 3)} res: {result_vec[i]}")
         rot = FreeCAD.Rotation(0, 0, 0)
-        if used_parts[i] in ["CE2", "EC2"]:
+        if parts_used[i] in ["CE2", "EC2"]:
             rot = FreeCAD.Rotation(0, -np.rad2deg(la), 0)
-        if used_parts[i] == "EE2":
-            rot = FreeCAD.Rotation(0, -np.rad2deg(la * 2), 0)
+        # if parts_used[i] == "EE2":          # there is no EE2
+        #     rot = FreeCAD.Rotation(0, -np.rad2deg(la * 2), 0)
         new_place = FreeCAD.Placement(result_vec[i], rot)
         lcs.Placement = lcs.Placement.multiply(new_place)
         # break